d4b266843d1c4ceb905e21d1adcf6a540162877c
150703398 <150703398@qq.com>
2026-01-13 10:09:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "[+] Starting RustDesk hbbr"
 hbbr &
-
-echo "[+] Starting RustDesk hbbs"
 hbbs -r 127.0.0.1:21117 &
-
-echo "[+] Starting Cloudflare Tunnel"
 cloudflared tunnel run --token "$CF_TUNNEL_TOKEN"
